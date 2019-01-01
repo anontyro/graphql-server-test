120b112bd0634614b86741bdd62006892f5b2ac3
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-01-01 14:47:45 +0800

    added in babel and nodemon, need to update to es6/7 and split out some of the code for future use

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const schema = require("./schemas/bggSchema");
+import schema from "./schemas/bggSchema";
 const app = express();
 
 app.use(
---
+++ src/schemas/index.js
@@ -0,0 +1,6 @@
+// import { mergeTypes } from "merge-graphql-schemas";
+// const bggSchema = require("./bggSchema");
+
+// const types = [bggSchema];
+
+// export default mergeTypes(types, { all: true });
